7bf73b2d2a1c9ff3cea6699f3fb5a623326e5403
bettingprashant <92102988+bettingprashant@users.noreply.github.com>
2025-05-15 12:09:55 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,3 +7,12 @@ pytest -v --tb=short --maxfail=5 --disable-warnings > pytest_output.log
 
 echo === Pytest Output ===
 type pytest_output.log
+@echo off
+echo === Activating Virtual Environment ===
+call venv\Scripts\activate.bat
+
+echo === Running Pytest ===
+pytest -v --tb=short --maxfail=5 --disable-warnings > pytest_output.log
+
+echo === Pytest Output ===
+type pytest_output.log
