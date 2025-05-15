bc1eb1911ada239260e46957387c70ab60784397
prashant raj <rajprashant9334@gmail.com>
2025-05-15 17:19:40 +0530

    sixth commit

--- run.bat
+++ run.bat
@@ -1,10 +1,14 @@
 @echo off
 echo === Running Pytest using virtual environment Python ===
 
-REM Activate virtual environment and run pytest with HTML report and log output
-.venv\Scripts\activate
+REM Go to script directory
+cd /d %~dp0
 
-pytest -v --tb=short --maxfail=5 --disable-warnings --html=Reports/report.html --self-contained-html > pytest_output.log
+REM Ensure Reports folder exists
+if not exist Reports mkdir Reports
+
+REM Run pytest using virtual environment Python
+".venv\Scripts\python.exe" -m pytest -v --tb=short --maxfail=5 --disable-warnings --html=Reports/report.html --self-contained-html > pytest_output.log
 
 echo === Pytest Output ===
 type pytest_output.log
